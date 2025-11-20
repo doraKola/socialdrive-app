931001b743aed295cf22217a68580e4354d701e9
Dor Kol <dork@okoora.com>
2025-11-20 09:47:11 +0200

    set list desgien

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,19 +1,8 @@
 import { Routes } from '@angular/router';
+import { ListComponent } from './features/links/list/list';
 
 export const routes: Routes = [
-  {
-    path: '',
-    redirectTo: 'links',
-    pathMatch: 'full'
-  },
-  {
-    path: 'links',
-    loadComponent: () =>
-      import('./features/links/list/list').then(m => m.ListComponent)
-  },{
-  path: 'links/add',
-  loadComponent: () =>
-    import('./features/links/add/add').then(m => m.AddComponent)
-}
-
+  { path: '', redirectTo: 'links', pathMatch: 'full' },
+  { path: 'links', component: ListComponent },
+  { path: 'links/add', loadComponent: () => import('./features/links/add/add').then(m => m.Add) },
 ];
